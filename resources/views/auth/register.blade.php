b0244f8137fb46a2b65a673164d25eac71828e4b
samuel-soto2 <1461888@senati.pe>
2023-10-26 14:41:58 -0500

    creacion de un login

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    Register
+</body>
+</html>

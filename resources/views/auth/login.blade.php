c095a3ee01d2623551fa8e40f8a7d7800f25e48c
samuel-soto2 <1461888@senati.pe>
2023-10-26 16:36:42 -0500

    entrar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bienvenido!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Bienvenido</h1>
                   </div>
                   <form action="{{ route('login.action') }}" method="POST" class="user">
                     @csrf
@@ -41,23 +41,23 @@
                       </div>
                     @endif
                     <div class="form-group">
-                      <input name="email" type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input name="email" type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="correo electrónico">
                     </div>
                     <div class="form-group">
-                      <input name="password" type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input name="password" type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña">
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
                         <input name="remember" type="checkbox" class="custom-control-input" id="customCheck">
-                        <label class="custom-control-label" for="customCheck">Remember
-                          Me</label>
+                        <label class="custom-control-label" for="customCheck">Visualizar
+                          </label>
                       </div>
                     </div>
                     <button type="submit" class="btn btn-primary btn-block btn-user">Login</button>
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="{{ route('register') }}">Create an Account!</a>
+                    <a class="small" href="{{ route('register') }}">Crear una cuenta</a>
                   </div>
                 </div>
               </div>

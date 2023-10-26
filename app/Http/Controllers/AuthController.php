0a72154fc713775216942b0f9357d6b33ae42d21
samuel-soto2 <1461888@senati.pe>
2023-10-26 16:25:37 -0500

    registro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
   
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -38,5 +39,39 @@ public function registerSave(Request $request)
         return view('auth/login');
     }
 
+    public function loginAction(Request $request)
+    {
+        Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ])->validate();
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            throw ValidationException::withMessages([
+                'email' => trans('auth.failed')
+            ]);
+        }
+  
+        $request->session()->regenerate();
+  
+        return redirect()->route('dashboard');
     }
+  
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+  
+        $request->session()->invalidate();
+  
+        return redirect('/');
+    }
+ 
+    public function profile()
+    {
+        return view('profile');
+    }
+}
+    
+
+    
 

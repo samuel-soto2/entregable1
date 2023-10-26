81fbfdbfdd35106d00ab7a8abf40abfedf517bda
samuel-soto2 <1461888@senati.pe>
2023-10-26 16:01:36 -0500

    completandoo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
   
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -34,6 +33,6 @@ public function registerSave(Request $request)
 
         return redirect()->route('login');
     }
-    
+
     }
 

79604ffe351d15d66d7ae4d0262aa753cae97122
samuel-soto2 <1461888@senati.pe>
2023-10-26 18:29:40 -0500

    acceso a la pagina

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,15 @@ Route::controller(AuthController::class)->group(function () {
 
     Route::get('login', 'login')->name('login');
     Route::post('login', 'loginAction')->name('login.action');
+
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
 });
 
 Route::middleware('auth')->group(function () {
     Route::get('dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/profile', [App\Http\Controllers\AuthController::class, 'profile'])->name('profile');
 });
     

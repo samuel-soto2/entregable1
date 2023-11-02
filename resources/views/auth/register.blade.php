34f3950c7ea95f992e819ce90a81baa22efe8b57
samuel-soto2 <1461888@senati.pe>
2023-11-02 08:25:42 -0500

    clinica buena salud

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,41 +24,41 @@
           <div class="col-lg-7">
             <div class="p-5">
               <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                <h1 class="h4 text-gray-900 mb-4">Crear una cuenta!</h1>
               </div>
               <form action="{{ route('register.save') }}" method="POST" class="user">
                 @csrf
                 <div class="form-group">
-                  <input name="name" type="text" class="form-control form-control-user @error('name')is-invalid @enderror" id="exampleInputName" placeholder="Name">
-                  @error('name')
+                  <input name="nombre" type="text" class="form-control form-control-user @error('nombre')is-invalid @enderror" id="exampleInputName" placeholder="nombre">
+                  @error('nombre')
                     <span class="invalid-feedback">{{ $message }}</span>
                   @enderror
                 </div>
                 <div class="form-group">
-                  <input name="email" type="email" class="form-control form-control-user @error('email')is-invalid @enderror" id="exampleInputEmail" placeholder="Email Address">
-                  @error('email')
+                  <input name="correo" type="correo" class="form-control form-control-user @error('correo')is-invalid @enderror" id="exampleInputEmail" placeholder="correo electrónico">
+                  @error('correo')
                     <span class="invalid-feedback">{{ $message }}</span>
                   @enderror
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input name="password" type="password" class="form-control form-control-user @error('password')is-invalid @enderror" id="exampleInputPassword" placeholder="Password">
-                    @error('password')
+                    <input name="contraseña" type="contraseña" class="form-control form-control-user @error('contraseña')is-invalid @enderror" id="exampleInputcontraseña" placeholder="contraseña">
+                    @error('contraseña')
                       <span class="invalid-feedback">{{ $message }}</span>
                     @enderror
                   </div>
                   <div class="col-sm-6">
-                    <input name="password_confirmation" type="password" class="form-control form-control-user @error('password_confirmation')is-invalid @enderror" id="exampleRepeatPassword" placeholder="Repeat Password">
-                    @error('password_confirmation')
+                    <input name="contraseña_confirmation" type="contraseña" class="form-control form-control-user @error('contraseña_confirmation')is-invalid @enderror" id="exampleRepeatcontraseña" placeholder="Repetir contraseña">
+                    @error('contraseña_confirmation')
                       <span class="invalid-feedback">{{ $message }}</span>
                     @enderror
                   </div>
                 </div>
-                <button type="submit" class="btn btn-primary btn-user btn-block">Register Account</button>
+                <button type="submit" class="btn btn-primary btn-user btn-block">Registrar cuenta</button>
               </form>
               <hr>
               <div class="text-center">
-                <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
+                <a class="small" href="{{ route('login') }}">Ya tienes una cuenta? Login!</a>
               </div>
             </div>
           </div>
